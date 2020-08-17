9790f07f44219a50aa7c73c0b90459797355a56c
Eljoenai Muninga <eolykab@yahoo.co.za>
2020-08-17 12:34:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vp_webservice_dotnet_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
